d2ec19f09979e16b85b93528278a803ac05f3cc5
Tajul Islam <tajul.azazil@gmail.com>
2021-03-29 17:52:09 +0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bodyParser = require('body-parser');
+
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 
